0922f8151bfcb53bf8edd022a8a3f12c65ad8418
Ondřej Nevole <ondrej.nevole@pslib.cz>
2022-03-24 08:09:14 +0100

    images

--- Imagesdk/Imagesdk/App.xaml.cs
+++ Imagesdk/Imagesdk/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Image_dk
+namespace Imagesdk
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ Imagesdk/Imagesdk/ViewModels/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Imagesdk.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public RelayCommand AddImage { get; set; }
+
+        public RelayCommand ShadesOfGrey { get; set; }
+
+        public RelayCommand Negative { get; set; }
+
+        public RelayCommand Mirror { get; set; }
+
+        public RelayCommand Reverse { get; set; }
+        public MainViewModel()
+        {
+            AddImage = new RelayCommand(() =>
+            {
+                var x = 0;
+            }
+            );
+
+            ShadesOfGrey = new RelayCommand(() =>
+            {
+                var x = 0;
+            }
+            );
+
+            Negative = new RelayCommand(() =>
+            {
+                var x = 0;
+            }
+            );
+
+            Mirror = new RelayCommand(() =>
+            {
+                var x = 0;
+            }
+            );
+
+            Reverse = new RelayCommand(() =>
+            {
+                var x = 0;
+            }
+);
+
+        }
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
